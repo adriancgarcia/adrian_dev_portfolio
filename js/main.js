cae445adadc0777a61ff49965cabb3cbe691ca07
adriancgarcia <adriancgarcia@gmail.com>
2024-01-15 22:29:48 -0600

    added hamburger menu but not sticking properly

--- js/main.js
+++ js/main.js
@@ -1,17 +1,30 @@
+function myFunction () {
+  var x = document.getElementById("section-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+window.onscroll = function() {scrollFunction()};
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementById("section-links");
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
+function scrollFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
   }
 }
+
+
+
+
